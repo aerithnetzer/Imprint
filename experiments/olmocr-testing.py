4d5a9bb316f1ed5e6cfbe3befd9b46ff8d86248c
Aerith Netzer <aerithnetzer@outlook.com>
2025-09-12 22:44:37 -0400

    update experiment

--- experiments/olmocr-testing.py
+++ experiments/olmocr-testing.py
@@ -1,9 +1,23 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
+from transformers import AutoProcessor, AutoModelForImageTextToText, pipeline
 
-pipe = pipeline("image-to-text", model="allenai/olmOCR-7B-0825-FP8")
-print(
-    pipe(
-        "/projects/p32234/projects/aerith/Imprint/test-images/35556036056489_00000002.jpg"
-    )
+model_id = "allenai/olmOCR-7B-0825-FP8"
+
+processor = AutoProcessor.from_pretrained(model_id)
+model = AutoModelForImageTextToText.from_pretrained(model_id, device_map="cuda")
+
+pipe = pipeline(
+    "image-to-text",
+    model=model,
+    tokenizer=processor.tokenizer,
+    image_processor=processor.image_processor,
+    device=0,
 )
+
+result = pipe(
+    {
+        "image": "/projects/p32234/projects/aerith/Imprint/test-images/35556036056489_00000002.jpg",
+        "prompt": "Please transcribe the text in this image.",
+    }
+)
+
+print(result)
